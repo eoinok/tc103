8c5ee565bc9baa45020049be55c2e15a8928cc96
Eoin OKennedy <Eoin.okennedy@tudublin.ie>
2024-02-28 22:33:36 +0000

    added Envoy.blade.php

---
+++ Envoy.blade.php
@@ -0,0 +1,36 @@
+@servers(['web' => ['{{ $host }}']])
+
+@story('deploy')
+    git-pull-code
+    database-migrate
+    composer-update
+    node-update
+    npm-run-prod
+@endstory
+
+@task('git-pull-code',  ['on' => 'web'])
+    cd `ls`
+    git pull origin main
+@endtask
+
+@task('database-migrate', ['on' => 'web'])
+    cd `ls`
+    php artisan migrate --force
+@endtask
+
+@task('composer-update', ['on' => 'web'])
+    cd `ls`
+    php composer.phar update --no-interaction --optimize-autoloader
+@endtask
+
+@task('node-update', ['on' => 'web'])
+    cd `ls`
+    source ~/.nvm/nvm.sh
+    npm install
+@endtask
+
+@task('npm-run-prod', ['on' => 'web'])
+    cd `ls`
+    source ~/.nvm/nvm.sh
+    npm run prod
+@endtask
